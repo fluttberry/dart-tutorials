c6432afb4ff1e43d2121b4cb31bfc82dce9b4615
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-03-22 23:19:25 +0600

    cycle for and foreach

---
+++ lesson10.dart
@@ -0,0 +1,13 @@
+void main (){
+    for (var i = 0; i < 5; i++) {
+    print('hi all' + i.toString());
+}
+
+for (var i = 1; i <= 5; i++) {
+    print('hi all' + i.toString());
+}
+
+for (var i = 1; i <= 5; i+=2) {
+    print('hi all' + i.toString());
+}
+}
