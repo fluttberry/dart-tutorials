f4ec1490186d46b572128aaefcfaadd9be112463
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-04-05 21:55:16 +0600

    callback, ananym functions

--- lesson13.dart
+++ lesson13.dart
@@ -1,21 +1,49 @@
-void main (){
-                          // this ananymys function
-//   Function sum (a, b) {
-//     return a + b;
-//   };                    
-// // unction sum (a, b) => a + b;
-  
-//   print(sum(2, 2));
+void main() {
+  // this ananymys function
+  //   Function sum (a, b) {
+  //     return a + b;
+  //   };
+  // // unction sum (a, b) => a + b;
 
-makeTask(doSomething);
+  //   print(sum(2, 2));
 
+  // makeTask(doSomething);
 
+  // }
+  // void makeTask (Function task){
+  // print('started the task');
+  // task();
+  // print('completed the task');
+
+  // }
+
+  // void doSomething() => print('hello');
+
+  final sum = (num a, num b) => a + b;
+  final diff = (num a, num b) => a - b;
+  final divide = (num a, num b) => a / b;
+  final mult = (num a, num b) => a * b;
+
+  print(calculate(1, 2, sum));
+  print(calculate(1, 2, diff));
+  print(calculate(1, 2, divide));
+  print(calculate(1, 2, mult));
 }
-void makeTask (Function task){
-print('started the task');
-task();
-print('completed the task');
 
-}
+calculate(num a, 
+num b, 
+num function(num a, num b), 
+action) => (a, b);
 
-void doSomething() => print('hello');
+    // switch (action){
+    //     case '+':
+    //     return a + b;
+    //     case '-':
+    //     return a - b;
+    //     case '/':
+    //     return a / b;
+    //     case '*':
+    //     return a * b;
+    //     default:
+    //     return 0;
+    // }
