e95306615b5e8393e93a95ceb38da605236284a2
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-04-21 23:35:41 +0600

    oop static fields

--- lesson21.dart
+++ lesson21.dart
@@ -1,22 +1,25 @@
-//THE MOST IMPORTANT #1
-
 void main() {
   var sanya = Human(name: 'Sasha', age: 25, height: 190);
 
-  print(sanya);
   print(sanya.name);
-  print(sanya.age);
-  print(sanya.height);
+  print(Human.count);
 
-  sanya.age = 28;
-  print(sanya.age);
+  var vova = Human(name: 'Vova', age: 25, height: 190);
+
+  print(vova.name);
+  print(Human.count);
+  
 }
 
 class Human {
   Human({
     required this.name, 
     required this.age, 
-    required this.height});
+    required this.height}){
+      count += 1;
+    }
+
+    static int count = 0;
     
   final String name; // final so name can not be changed and printed
   num age;
